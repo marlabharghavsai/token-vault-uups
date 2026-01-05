d2a3cc66fc2b270b493b220f890628537bdc9703
marlabharghavsai <marlabharghavsai@gmail.com>
2026-01-05 14:28:56 +0000

    test: TokenVaultV1 core functionality

---
+++ test/TokenVaultV1.test.js
@@ -0,0 +1,8 @@
+describe("TokenVaultV1", function () {
+  it("should initialize with correct parameters", async function () {});
+  it("should allow deposits and update balances", async function () {});
+  it("should deduct deposit fee correctly", async function () {});
+  it("should allow withdrawals and update balances", async function () {});
+  it("should prevent withdrawal of more than balance", async function () {});
+  it("should prevent reinitialization", async function () {});
+});
